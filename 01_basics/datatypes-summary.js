b0dae7dbf7730c0db0eb8e72221938e2f6d9768a
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-26 14:15:24 +0000

    Data type Summary.

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,28 @@
+// Two types of Data Types 
+
+// Primitive : 7 types => Number, String, Boolean, Bigint, Symbol, undefined, null 
+
+
+// Non-Primitive : 3 Types => Array, Object, Functions 
+
+
+// Array 
+
+// let firstArray = ["name", 12, "state", "country"] 
+
+// Object
+
+// let firstObj = {
+//     name: "test",
+//     number: 12,
+//     state: "test-state",
+//     country: "test-country"
+// }
+
+// Functions 
+
+let myFunctions = function(){
+    console.log("test");
+}
+
+console.log("myFunctions");
