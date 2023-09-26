8dd1547a4d48eb308971f04f812d13398c66c940
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-26 11:56:17 +0000

    Understand Conversions of Datatypes in JS.

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,38 @@
+let score = "33aaa"
+
+// console.log(score); 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score); //converted into Number
+
+// console.log(typeof valueInNumber);
+
+//console.log(valueInNumber); // Nan Not a Number, for null we get 0 and for undefined we get NaN. type of Nan is number.
+
+// 33 => number
+// "33" => string
+// "33aa" => Nan
+// null => 0 that is number
+// undefined => Nan
+// true => 1
+//false => 0
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn) // converted into Boolean
+
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => flase; "test" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) //converted into String
+
+console.log(typeof stringNumber);
+
+
