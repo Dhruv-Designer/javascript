ce7d6293e79f627fecd6fe5c4e87f8d9b175319f
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-26 11:22:40 +0000

    Understanding Datatypes in JS.

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all code as newer version
+
+// alert( 3 + 3 );  not work bcz we work in node not in browser.
+
+// number => 2 to power of 52 
+// string => ""
+// bigint => large number 
+// Boolean => true/false 
+// null => standalone value 
+// undefined 
+// symbol => unique components 
+
+console.log(typeof null); // type of null is OBJECT
+
+console.log(typeof undefined); //type of undefined is UNDEFINED
+
+console.log(typeof "test"); //type of test is string
