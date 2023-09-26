15b6dcf4a2672f5e7840faf055afc692f2783ca2
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-26 10:53:59 +0000

    Variables in Js.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14432
+let accountEmail = "test@gmail.com"
+var accountPassword = 12345
+accountCity = "Surat"
+let accountState;
+
+// accountId = 14533  Not allowed bcz it is CONSTANT.
+
+// console.log(accountId);
+
+// let is used to declare variable in JSON={..var is not used in latest js bcz it conflict in block scope and functional scope 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
