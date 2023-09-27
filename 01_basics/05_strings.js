3b05ef4a8ef0da07360435458db180bf873be885
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-27 04:53:15 +0000

    More strings added 2.0

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,14 +8,29 @@ let Lname = "Ltets"
 
 const gameName = new String("Dhruv   ");
 
-console.log(gameName [0]); // D
-console.log(gameName.length); // 5
-console.log(gameName.toLocaleLowerCase()); // dhruv
+// console.log(gameName [0]); // D
+// console.log(gameName.length); // 5
+// console.log(gameName.toLocaleLowerCase()); // dhruv
 
-console.log(gameName.charAt([4])); //v
+// console.log(gameName.charAt([4])); //v
+// console.log(gameName.indexOf('r')); //2
 
-const newGameName = gameName.trim()
-const gameNameSubString = gameName.substring(0,3)
+const newGameName = gameName.trim() // trim space from start and end both.
+// console.log(newGameName); // Dhruv (trim space from start & end)
 
-console.log(newGameName); // Dhruv (trim space from start & end)
-console.log(gameNameSubString); // Dhr
+
+const gameNameSubString = gameName.substring(0,3) // Counts from 0-2 ..3rd index not printed.
+// console.log(gameNameSubString); // Dhr
+
+
+const  anotherString = gameName.slice(0,4) // you can give negative value in slice also.
+// console.log(anotherString);
+
+const url = "https://dhurv.com/dhruv%20patel"
+// console.log(url.replace('%20','-'));  // ir used to replace something in strings
+
+// console.log(url.includes('Dhurv')); // false - check given value in string...case sensitive
+
+const splitString = "test-test1.0-test2.0"
+console.log(splitString.split('-','2'));
+
