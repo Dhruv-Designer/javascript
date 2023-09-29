90cc67053a7bb8d83bdbb94ebe97fa0dcaddda55
Dhruv-Designer <144476062+Dhruv-Designer@users.noreply.github.com>
2023-09-29 11:42:52 +0000

    Switch in JS.

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 10
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Enter valid month.")
+        break;
+}
