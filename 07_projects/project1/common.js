7bdb2477c6a2fe7407da2d3ce6b683413cf4cc5d
Dhruv-Designer <primetroubleshooter@gmail.com>
2023-10-03 12:42:56 +0530

    Project 1 Completed.

--- 07_projects/project1/common.js
+++ 07_projects/project1/common.js
@@ -1,6 +1,46 @@
-const button = document.querySelectorAll(".buttton")
+const button = document.querySelectorAll(".button")
 const body = document.querySelector("body")
 
-button.forEach( (button) => {
+button.forEach( function(button) {
+    button.addEventListener('click', function(e){
+        // console.log(e);
+        // console.log(e.target.id);
 
+        if (e.target.id === "grey") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "white") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "blue") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "yellow") {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === "dark") {
+            body.style.backgroundColor = "#252525"
+        }
+
+        // switch (e.target.id) {
+        //     case "grey" :
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case "white" :
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case "blue" :
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case "yellow" :
+        //         body.style.backgroundColor = e.target.id
+        //         break;
+        //     case "dark" :
+        //         body.style.backgroundColor = "#d2d2d2"
+        //         break;
+        
+        //     default:
+        //         break;
+        // }
+    })
 } )
